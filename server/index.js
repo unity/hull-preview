c8e806aa81072790e665c2c20dfd973ea0b4d8fd
Michal Raczka <michal@raczka.me>
2017-03-10 12:26:38 +0100

    Load all properties and render them

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import Hull from "hull";
 import express from "express";
 import cors from "cors";
+import _ from "lodash";
 
 const connector = new Hull.Connector({
   port: process.env.PORT || 8082,
@@ -11,15 +12,27 @@ const app = express();
 connector.setupApp(app);
 
 app.get("/preview", cors(), (req, res, next) => {
+  const { client } = req.hull;
   const ident = JSON.parse(Buffer.from(req.query.ident, "base64").toString());
-  req.hull.client.logger.info("loading user", ident);
-  const scopedHull = req.hull.client.as(ident);
+  client.logger.info("loading user", ident);
+  const scopedHull = client.as(ident);
 
   return scopedHull.get("/me")
   .then(user => {
-    return scopedHull.get(`/${user.id}/segments`)
-      .then(segments => {
-        return res.render("home.html", { user, segments });
+    return client.get(`/${user.id}/user_report`)
+      .then(userData => {
+
+        const grouped = client.utils.groupTraits(userData);
+        const userForView = {
+          properties: {}
+        };
+        _.map(grouped, (element, key) => {
+          if (_.isObject(element) && !_.isArray(element)) {
+            return userForView[key] = element;
+          }
+          return userForView.properties[key] = element;
+        });
+        return res.render("home.html", { user: userForView, _ });
       });
   })
   .catch(err => {
